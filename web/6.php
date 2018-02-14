a5e2ab78d0d4ffd12120492ed07826506ced37ed
keviljh3 <kevinljh3@hotmail.com>
2018-02-14 19:46:26 +0800

    Update 6.php

--- web/6.php
+++ web/6.php
@@ -1,4 +1,4 @@
-<?
+<?php
 ini_set('user_agent','Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.22 (KHTML, like Gecko) QQBrowser/5.6.1 Safari/537.22'); 
 error_reporting(E_ALL ^ E_NOTICE);
 ini_set('date.timezone','Asia/Shanghai'); 
